e21246b20bf941c3cff752e340d3d38a57d16c1f
Bruno ROCHA PEREIRA <b.rocha.per@gmail.com>
2020-04-09 10:47:29 +0200

    Updates

---
+++ precache-manifest.ee4efbd26c3394f4bbf9af2330a55c2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f16bd7442da8c7ef16e04fd3ac3c3d",
+    "url": "/clicker-client/index.html"
+  },
+  {
+    "revision": "c8f7a8585dadf5af252d",
+    "url": "/clicker-client/static/css/main.77b40c47.chunk.css"
+  },
+  {
+    "revision": "bea828b62df36979d74e",
+    "url": "/clicker-client/static/js/2.c4a5183e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicker-client/static/js/2.c4a5183e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f7a8585dadf5af252d",
+    "url": "/clicker-client/static/js/main.41f5b1fa.chunk.js"
+  },
+  {
+    "revision": "6afe8da5fe29ed3723f1",
+    "url": "/clicker-client/static/js/runtime-main.4478585a.js"
+  }
+]);
